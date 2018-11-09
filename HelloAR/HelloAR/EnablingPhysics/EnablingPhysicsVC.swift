890b425ca4aacbcf3a143638de75dfafb5a6eada
강수진 <rkdthd1234@naver.com>
2018-11-09 17:05:39 +0900

    EnablingPhysicsVC 생성 노드에 물리법칙 적용

---
+++ HelloAR/HelloAR/EnablingPhysics/EnablingPhysicsVC.swift
@@ -0,0 +1,117 @@
+//
+//  EnablingPhysicsVC.swift
+//  HelloAR
+//
+//  Created by 강수진 on 2018. 11. 9..
+//  Copyright © 2018년 강수진. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class EnablingPhysicsVC: UIViewController, ARSCNViewDelegate {
+
+    var sceneView: ARSCNView!
+    var planes = [OverlayPlane]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView = ARSCNView(frame : self.view.frame)
+        self.view.addSubview(sceneView)
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        registerGestureRecognizers()
+    }
+    
+    
+    private func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        
+        if !hitTestResult.isEmpty {
+            
+            guard let hitResult = hitTestResult.first else {
+                return
+            }
+            addBox(hitResult :hitResult)
+        }
+    }
+    private func addBox(hitResult :ARHitTestResult) {
+        
+        let boxGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        boxGeometry.materials = [material]
+        
+        let boxNode = SCNNode(geometry: boxGeometry)
+     
+        //physicsBody 적용한 부분
+        boxNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        boxNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,hitResult.worldTransform.columns.3.y + Float(0.5), hitResult.worldTransform.columns.3.z)
+
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if !(anchor is ARPlaneAnchor) {
+            return
+        }
+        
+        let plane = OverlayPlane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+            }.first
+        
+        if plane == nil {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
